d703af6a7230aa550177d34ed5e47d28f861d53c
Jean Minnaar <Jean_Minnaar@hotmail.com>
2018-11-05 11:35:24 -0700

    Azure Search

---
+++ AzureSearch.Search/Loader.cs
@@ -0,0 +1,16 @@
+﻿using AzureSearch.Common;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureSearch.Search
+{
+    public class Loader
+    {
+        public static async Task Update()
+        {
+
+        }
+
+    }
+}
